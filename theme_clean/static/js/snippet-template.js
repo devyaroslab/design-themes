2c7066509b87f4eafc13c5867da3cdacefdf9e96
Antony Lesuisse <lesuisse@gmail.com>
2014-06-26 15:55:47 +0200

    m

---
+++ theme_clean/static/js/snippet-template.js
@@ -0,0 +1,20 @@
+(function() {
+  'use strict';
+  var website = openerp.website;
+  website.openerp_website = {};
+  website.snippet.animationRegistry.snippet_name = website.snippet.Animation.extend({
+  
+    selector : ".SNIPPET-NAME",
+    
+    start: function(){
+     
+	 <!-- Start Custom JS Here -->
+	 
+
+	 
+	 <!-- Stop Custom JS Here -->
+	 
+    },
+  
+  });
+})();
